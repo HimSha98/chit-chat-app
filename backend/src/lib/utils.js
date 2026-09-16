7540c665d3da265810ada0dd817a821bfcc5c98a
Himanshu Sharma <himanshu.sharma.1013@gmail.com>
2026-09-16 23:30:11 +0530

    HS 16092026 SET UP ARCJET FOR THE RATE LIMITING, BOT DETECTION AND SO ON

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -18,7 +18,7 @@ export const generateToken = (userId, res) => {
     });
 
     res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 60 * 1000, // HS FOR SEVEN DAYS IN MILLISECONDS
+        maxAge: 7 * 24 * 60 * 60 * 1000, // HS FOR SEVEN DAYS IN MILLISECONDS
         httpOnly: true, // HS PREVENT XSS ATTACKS, THE TOKEN CAN BE ACCESSIBLE ONLY THROUGH HTTP, WITH JS NO ONE CAN ACCESS THIS
         sameSite: "strict", // HS PREVENT CSRF ATTACKS
         // HS THIS SECURE VALUE DEPENDS ON HTTP FOR DEVELOPMENT AND HTTPS FOR PRODUCTION
